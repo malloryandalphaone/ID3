114c55fe3403fc710d4c4d1f4433f7950afa23c7
malloryandalphaone <42677997+malloryandalphaone@users.noreply.github.com>
2019-05-11 21:49:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,15 @@ const moment = require("moment");
 const ms = require("ms");
 const wait = require('util').promisify(setTimeout);
 client.on('ready', () => {
-var x = client.channels.get("576009269984493568");
+var x = client.channels.get("576394046214242304");
 if (x) x.join();
+});
+
+client.on("ready", () => {
+client.user.setStatus('online');
+//client.user.setGame("- Atom.", "https://www.twitch.tv/idk");
+  console.log("Reeebel | Logged in! Server count: ${client.guilds.size}");
+ // client.user.setActivity("Royal Orders.",{type: 'WATCHING'});
 });
 
 
